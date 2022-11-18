b5cf417e78caeffe98c8fcedd97c84c65aec3d0d
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2022-11-18 17:27:42 +0500

    Done

--- lib/home.dart
+++ lib/home.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
-        title: const Text('PAYMENT METHOD '),
+        title: const Text('PAYMENT METHOD (STRIPE) '),
         centerTitle: true,
         toolbarHeight: 80,
       ),
