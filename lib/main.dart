cafae00a0ed25ab12e55578a4b8f2d7d0ed39829
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-02-06 22:26:06 +0500

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Stripe',
+      title: 'Flutter Stripe Gateway',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
